2bb5c0f2b036980fe0b7d14ea64d7eb2e62edd5f
HunSeok Sun <xodidxkq@naver.com>
2026-01-18 14:29:41 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [[0] * 100 for _ in range(100)]
+
+for _ in range(n):
+    x, y = map(int, input().split())
+
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            arr[i][j] = 1
+
+res = 0
+for k in range(100):
+    res += sum(arr[k])
+
+print(res)
