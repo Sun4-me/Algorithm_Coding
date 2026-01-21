18374388dc3f52ee906a06011b4cef7d5ded2111
HunSeok Sun <xodidxkq@naver.com>
2026-01-21 12:29:30 +0900

    [Bronze I] Title: 이항 계수 1, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+
+
+def fac(a):
+    res = 1
+    for i in range(2, a + 1):
+        res *= i
+    return res
+
+
+def combi(b, c):
+    return fac(b) // (fac(b - c) * fac(c))
+
+
+print(combi(n, k))
