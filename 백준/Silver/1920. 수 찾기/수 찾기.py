901c3b8d2dde08d58e479211ac5e4f1fbc0ce703
HunSeok Sun <xodidxkq@naver.com>
2026-01-22 10:03:23 +0900

    [Silver IV] Title: 수 찾기, Time: 176 ms, Memory: 145300 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,10 @@
+n = int(input())
+n_nums = set(map(int, input().split()))
+m = int(input())
+m_nums = list(map(int, input().split()))
+
+for num in m_nums:
+    if num in n_nums:
+        print(1)
+    else:
+        print(0)
