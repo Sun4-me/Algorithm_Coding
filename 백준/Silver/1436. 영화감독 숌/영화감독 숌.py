2274bd141beff8da1a989ca2957b579c2120e60e
HunSeok Sun <xodidxkq@naver.com>
2026-01-22 11:55:14 +0900

    [Silver V] Title: 영화감독 숌, Time: 172 ms, Memory: 110728 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+num = 666
+cnt = 0
+
+while True:
+    if '666' in str(num):
+        cnt += 1
+
+    if cnt == n:
+        print(num)
+        break
+
+    num += 1
+    
