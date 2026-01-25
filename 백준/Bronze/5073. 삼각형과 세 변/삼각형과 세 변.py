0f58217afd6722c7289d541fe5533e68e48e80c8
HunSeok Sun <xodidxkq@naver.com>
2026-01-25 18:34:23 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -0,0 +1,18 @@
+while True:
+
+    sides = list(map(int, input().split()))
+
+    if sum(sides) == 0:
+        break
+
+    if max(sides) >= sum(sides) - max(sides):
+        print("Invalid")
+
+    elif sides[0] == sides[1] == sides[2]:
+        print("Equilateral")
+
+    elif sides[0] == sides[1] or sides[1] == sides[2] or sides[2] == sides[0]:
+        print("Isosceles")
+
+    else:
+        print("Scalene")
