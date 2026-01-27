f1df5b6219212b418852f11fc2ecc6ae5079e8ab
HunSeok Sun <xodidxkq@naver.com>
2026-01-27 22:53:53 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 484 ms, Memory: 610124 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(300000)
+
+
+def dfs(x):
+    global order
+    # 방문 체크
+    visited[x] = order
+    order += 1
+
+    for nxt in adj[x]:
+        if visited[nxt] == 0:
+            dfs(nxt)
+
+
+n, m, r = map(int, input().split())
+nodes = [list(map(int, input().split())) for _ in range(m)]
+
+adj = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+# 인접 행렬 생성
+for row in nodes:
+    adj[row[0]].append(row[1])
+    adj[row[1]].append(row[0])
+
+# 내림 차순
+for row in adj:
+    row.sort(reverse=True)
+
+order = 1
+dfs(r)
+
+for i in range(1, n + 1):
+    print(visited[i])
