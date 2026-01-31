3db301d8f16579c0886f6a358d8362f659343325
HunSeok Sun <xodidxkq@naver.com>
2026-01-31 12:16:19 +0900

    [Gold V] Title: 토마토, Time: 480 ms, Memory: 220276 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,7 +1,4 @@
 from collections import deque
-import sys
-
-input = sys.stdin.readline
 
 #  위 아래 왼쪽 오른쪽 앞 뒤
 dx = [0, 0, -1, +1, 0, 0]
@@ -19,30 +16,33 @@ def bfs():
                 if grid[nz][ny][nx] == 0:
                     grid[nz][ny][nx] = grid[cur_z][cur_y][cur_x] + 1
                     q.append((nz, ny, nx))
-#################################################
+
+
+########################################
 m, n, h = map(int, input().split())
 grid = []
 for _ in range(h):
     grid.append([list(map(int, input().split())) for _ in range(n)])
-#################################################
+########################################
+# 미리 다 넣어 놓기
 q = deque()
 
-for depth in range(h):
+for z in range(h):
     for row in range(n):
         for col in range(m):
-            if grid[depth][row][col] == 1:
-                q.append((depth, row, col))
-#################################################
+            if grid[z][row][col] == 1:
+                q.append((z, row, col))
+
 bfs()
 #################################################
-# 토마토가 모두 익었는지 확인
+# 토마토 익었는지 확인하자
 res = 0
-for i in range(h):
-    for j in range(n):
-        for k in range(m):
-            if grid[i][j][k] == 0:
+for z in range(h):
+    for row in range(n):
+        for col in range(m):
+            if grid[z][row][col] == 0:
                 print(-1)
                 exit()
-            res = max(res, grid[i][j][k])
+            res = max(res, grid[z][row][col])
 
 print(res - 1)
