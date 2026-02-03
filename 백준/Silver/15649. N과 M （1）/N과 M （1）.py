f92d09ea86a0e553d6e34f7cb6374f4cf27cc0df
HunSeok Sun <xodidxkq@naver.com>
2026-02-03 12:25:03 +0900

    [Silver III] Title: N과 M (1), Time: 156 ms, Memory: 114144 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,20 @@
 def dfs(n, lst):
-    # 종료 조건 처리 + 정답 처리
-    if n == M:  # M개의 수열 완성
-        ans.append(lst)
+    if n == M + 1:  # M개 고르기
+        print(*lst)
         return
 
-    # 하부 함수 호출
-    for i in range(1, N + 1):
+    for i in range(1, N + 1):  # 1부터 N까지의 자연수
         if v[i] == 0:
-            v[i] = 1
-            dfs(n + 1, lst + [i])
-            v[i] = 0
+            v[i] = 1  # 방문 체크
+            dfs(n + 1, lst + [i])  # 방문 체크한거 들고 가서 방문 안한 곳만 방문하게 됨
+            v[i] = 0  # 다른 조합도 찾아야하니 방문 해제
 
 
+# N까지 중복없이 M개 고르기
 N, M = map(int, input().split())
-ans = []
+
+# 중복 없이 하게 하기 위해 Visted 배열 이용
 v = [0] * (N + 1)
 
-dfs(0, [])
-for lst in ans:
-    print(*lst)
+# 1 부터시작, 빈리스트 제공
+dfs(1, [])
