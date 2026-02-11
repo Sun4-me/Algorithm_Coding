3132128b4e123c9d1a96c7d818546745d3af58bf
HunSeok Sun <xodidxkq@naver.com>
2026-02-11 17:04:23 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 120 ms, Memory: 118756 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,12 +1,41 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+from collections import deque
+
+
+def bfs():
+    global n, k
+    v = [0] * 100001
+    v[n] = 1
+
+    q = deque([n])
+
+    cnt = 0
+    time = 0
+
+    while q:
+        curr = q.popleft()
+
+        if v[k]!= 0 and v[curr] > v[k]:
+            continue
+
+        if curr == k:
+            if time == 0:
+                time = v[curr]
+            cnt += 1
+            continue
+
+
+        for nxt in (curr - 1, curr + 1, curr * 2):
+            if 0 <= nxt <= 100000:
+                if v[nxt] == 0 or v[nxt] == v[curr] + 1:
+                    v[nxt] = v[curr] + 1
+                    q.append(nxt)
+
+
+    return cnt, time
+
+
+n, k = map(int, input().split())
+cnt, time = bfs()
+
+print(time - 1)
+print(cnt)
