adf73b57c48ab4350f0660f15ba662248095af39
HunSeok Sun <xodidxkq@naver.com>
2026-02-15 15:36:29 +0900

    [Silver II] Title: N과 M (12), Time: 124 ms, Memory: 111632 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,21 @@
+def dfs(depth, s):
+    if depth == m:
+        print(*lst)
+        return
+
+    used = 0
+
+    for i in range(s, n):
+        if nums[i] != used:
+            lst.append(nums[i])
+            used = nums[i]
+            dfs(depth + 1, i)
+            lst.pop()
+
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+lst = []
+dfs(0, 0)
