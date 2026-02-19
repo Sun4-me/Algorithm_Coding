b58a3361eb69046c2bf6b011e82a78d82965d367
HunSeok Sun <xodidxkq@naver.com>
2026-02-19 16:05:02 +0900

    [Gold IV] Title: 연구소, Time: 272 ms, Memory: 118120 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,63 +1,77 @@
-from collections import deque
-from itertools import combinations
-# from pprint import *
-# 모든 경우의 수 로 벽 3개 다 그려보고
-# 그때마다 2 위치 넣어둔 큐 넣고 돌려보고 최대값 갱신
+# 불 2, 방화벽 1, 빈칸 0
+# n, m 이 매우 작음 모든 경우 다 해보자
+# 빈 칸인 경우에만 방화벽 설치가능
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
+from itertools import combinations
+from collections import deque
 
 
 def bfs():
+    cnt = fire_cnt
+
     while q:
         cy, cx = q.popleft()
 
-        for k in range(4):
-            ny, nx = cy + dy[k], cx + dx[k]
+        for dy, dx in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            ny, nx = cy + dy, cx + dx
             if 0 <= ny < n and 0 <= nx < m:
-                if v[ny][nx] == 0 and grid[ny][nx] == 0:
-                    v[ny][nx] = 1
+                if grid[ny][nx] == 0 and v[ny][nx] == 0:
                     grid[ny][nx] = 2
+                    v[ny][nx] = 1
+                    cnt += 1
                     q.append((ny, nx))
 
+    return cnt
 
-#############################################################################
+
+###########################################################
 n, m = map(int, input().split())
 ori_grid = [list(map(int, input().split())) for _ in range(n)]
-#############################################################################
-coords = [(y, x) for x in range(m) for y in range(n) if ori_grid[y][x] == 0]
-can_wall = list(combinations(coords, 3))
-#############################################################################
-virus = []
+###########################################################
+# 빈칸인 좌표 모음
+coords = [(y, x) for y in range(n) for x in range(m) if ori_grid[y][x] == 0]
+
+# 가능한 3가지 좌표의 경우의 수
+possible = list(combinations(coords, 3))
+###########################################################
+# 불 좌표 찾아 두기
+# 빈칸 개수 찾아 두기
+ori_v = [[0] * m for _ in range(n)]
+
+fire = []
+wall_cnt = 0
 
 for y in range(n):
     for x in range(m):
         if ori_grid[y][x] == 2:
-            virus.append((y, x))
+            fire.append((y, x))
+            ori_v[y][x] = 1
 
-#############################################################################
-ans = 0
-for wall in can_wall:
-    cnt = 0
+        elif ori_grid[y][x] == 1:
+            wall_cnt += 1
 
-    grid = [lst[:] for lst in ori_grid]
+fire_cnt = len(fire)
+###########################################################
+min_cnt = 10 ** 10
 
+for case in possible:
+    # 임시 배열 생성 및 q,v 초기화
+    v = [row[:] for row in ori_v]
+    grid = [row[:] for row in ori_grid]
+    q = deque(fire)
+
+    # 벽 생성
     for i in range(3):
-        grid[wall[i][0]][wall[i][1]] = 1
+        now = case[i]
+        now_y, now_x = now[0], now[1]
+        grid[now_y][now_x] = 1
 
-    q = deque(virus)
-    v = [[0] * m for _ in range(n)]
+    # 불 퍼트리기
+    cnt = bfs()
 
-    for y, x in virus:
-        v[y][x] = 1
+    # 불을 퍼트린 영역의 수 탐색 및 가장 적게 퍼트린 영역 저장
+    min_cnt = min(min_cnt, cnt)
 
-    bfs()
-
-    for row in range(n):
-        for col in range(m):
-            if grid[row][col] == 0:
-                cnt += 1
-
-    ans = max(ans, cnt)
-#############################################################################
-print(ans)
+# 전체크기에서 불을 가장 적게 퍼트린 영역과 방화벽의수, 추가한 방화벽 3을 빼면
+# 불이 퍼지지 않는 영역크기의 최댓값임.
+print(n * m - (min_cnt + wall_cnt + 3))
