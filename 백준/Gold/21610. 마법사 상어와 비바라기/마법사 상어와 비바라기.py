bfce79bff4c11743394b7b3f18234d7e8d7b1e8b
HunSeok Sun <xodidxkq@naver.com>
2026-03-27 19:42:51 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 152 ms, Memory: 112652 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,56 +1,75 @@
-import sys
-
-input = sys.stdin.readline
-
+#############################################################
 dy = [0, -1, -1, -1, 0, 1, 1, 1]
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 
 ddy = [-1, -1, 1, 1]
 ddx = [-1, 1, -1, 1]
+#############################################################
+#############################################################
+# 격자 크기, 키우는 년수
+N, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+command = [list(map(int, input().split())) for _ in range(M)]
+#############################################################
+# 초기 영양제의 위치
+special = [[N - 1, 0], [N - 2, 0], [N - 1, 1], [N - 2, 1]]
 
-##################################################################
-n, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
-commands = [list(map(int, input().split())) for _ in range(m)]
-##################################################################
-clouds = [[n - 1, 0], [n - 1, 1], [n - 2, 0], [n - 2, 1]]
+for cmd in command:
+    # 영양제 투입 여부 배열
+    v = [[0] * N for _ in range(N)]
 
-for d, s in commands:
-    d = d - 1
+    # 이동 방향, 이동 칸수
+    d, p = cmd[0] - 1, cmd[1]
 
-    v = [[False] * n for _ in range(n)]
+    # 현재 영양제의 수
+    length = len(special)
 
-    # 모든 구름 d방향으로 s칸 이동
-    for i in range(len(clouds)):
-        clouds[i][0], clouds[i][1] = (clouds[i][0] + (dy[d] * s)) % n, (clouds[i][1] + (dx[d] * s)) % n
+    # 특수 영양제 이동
+    for i in range(length):
+        cy, cx = special[i][0], special[i][1]
 
-    # 구름이 있는 칸의 바구니 저장된 물 양 1증가
-    # 방문 처리
-    for y, x in clouds:
-        v[y][x] = True
-        grid[y][x] += 1
+        # 지구처럼 돌아오니까 처리
+        ny, nx = (cy + (dy[d] * p)) % N, (cx + (dx[d] * p)) % N
 
-    for y, x in clouds:
-        # 대각선 방향으로 거리가 1인 칸 수만큼 물 양 증가
+        # 이동 갱신
+        special[i][0], special[i][1] = ny, nx
+
+        # 방문 처리
+        v[ny][nx] = 1
+
+    # [2] (현재 높이 +1) 특수 영양제 투입된 나무 성장 시키기
+    for i in range(length):
+        cy, cx = special[i][0], special[i][1]
+        grid[cy][cx] += 1
+
+    # [2] (대각선) 특수 영양제 투입된 나무 성장 시키기
+    for i in range(length):
+        cy, cx = special[i][0], special[i][1]
+        # 대각선으로 인접한 방향에 높이가 1 이상인 리브로수 찾기
+
+        cnt = 0  # 성장할 높이
         for k in range(4):
-            ny, nx = y + ddy[k], x + ddx[k]
-            if 0 <= ny < n and 0 <= nx < n:
+            ny, nx = cy + ddy[k], cx + ddx[k]
+            if 0 <= ny < N and 0 <= nx < N:
                 if grid[ny][nx] >= 1:
-                    grid[y][x] += 1
+                    cnt += 1
 
-    # 구름 삭제
-    clouds = []
+        # 높이 증가
+        grid[cy][cx] += cnt
 
-    for y in range(n):
-        for x in range(n):
+    # 특수 영양제 사라짐
+    special = []
+
+    # 해당 년도에 특수 영양제를 맞은 땅을 제외하고
+    # 높이가 2이상인 리브로수를 높이 2만큼 잘라내고
+    # 해당 땅에 특수 영양제 올려주기
+    for y in range(N):
+        for x in range(N):
             if grid[y][x] >= 2:
                 if not v[y][x]:
-                    clouds.append([y, x])
                     grid[y][x] -= 2
+                    special.append([y, x])
 
-total = 0
-for y in range(n):
-    for x in range(n):
-        total += grid[y][x]
 
-print(total)
+# 높이 총합 출력
+print(sum(sum(row) for row in grid))
