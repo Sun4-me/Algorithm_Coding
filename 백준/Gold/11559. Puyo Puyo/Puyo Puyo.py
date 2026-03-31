e5e847942359e28388c170c2509f85a6441bfcdd
HunSeok Sun <xodidxkq@naver.com>
2026-03-31 19:52:08 +0900

    [Gold IV] Title: Puyo Puyo, Time: 120 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -1,82 +1,68 @@
-# import sys
 from collections import deque
 
 
-from pprint import *
-
-
-# 전치하고 리버스해서 첫 열만 확인하자
-# 매 사이클 첫 열만 확인, 방문 초기화
-# 방문이 안된 색깔 있는 좌표만 탐색
-# 해당 행에 append 빈칸 수행하고. pop 할 색깔들 큰것 부터 pop
-
-def bfs(cy, cx, color):
-    global now_cnt, ans, flag
-    v[cy][cx] = 1
-    q = deque([(cy, cx)])
-
-    ###########################
-    now_pop = []
-    now_pop.append((cy, cx))
-    ###########################
+def bfs(y, x):
+    global game
+    v[y][x] = True
+    q = deque([(y, x)])
+    ttemp = [(y, x)]
 
     while q:
         cy, cx = q.popleft()
+        cnum = grid[cy][cx]
 
         for dy, dx in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             ny, nx = cy + dy, cx + dx
-            if 0 <= ny < 6 and 0 <= nx < 12:
-                if v[ny][nx] == 0:
-                    if grid[ny][nx] == color:
-                        v[ny][nx] = 1
-                        now_cnt += 1  # 방문 수 카운트
-                        now_pop.append((ny, nx))  # 지울 좌표 +
-                        q.append((ny, nx))
+            if not (0 <= ny < N and 0 <= nx < M): continue
+            if v[ny][nx]: continue
+            if grid[ny][nx] == cnum:
+                v[ny][nx] = True
+                q.append((ny, nx))
+                ttemp.append((ny, nx))
 
-    # 4번이상인 경우에만 전역 변수에 업데이트
-    if now_cnt >= 4:
-        pop_cord.extend(now_pop)
-        flag = True
+    if len(ttemp) >= 4:
+        game = True
+        coord.extend(ttemp)
 
 
-############################################################
-original_grid = [list(input()) for _ in range(12)]
-# 전치 + reverse
-grid = [lst[::-1] for lst in list(map(list, zip(*original_grid)))]
-############################################################
+tmp = [list(input()) for _ in range(12)]
+grid = [lst[::-1] for lst in list(map(list, zip(*tmp)))]
+N, M = 6, 12
 ans = 0
-count = 0
 
 while True:
-    if count == 30:
-        break
+    game = False
 
-    count += 1
+    v = [[False] * M for _ in range(N)]
+    coord = []
 
-    # 지울 좌표 목록
-    pop_cord = []
+    for y in range(N):
+        for x in range(M):
+            if grid[y][x] != '.' and not v[y][x]:
+                bfs(y, x)
 
-    # 이번 턴에 성공
-    flag = False
+    for y, x in coord:
+        grid[y][x] = '.'
 
-    v = [[0] * 12 for _ in range(6)]
+    nxt_grid = [[] for _ in range(N)]
 
-    for row_idx in range(6):
-        for col_idx in range(12):
-            now_cnt = 1
-            if grid[row_idx][col_idx] != '.':
-                if v[row_idx][col_idx] == 0:
-                    color = grid[row_idx][col_idx]
-                    bfs(row_idx, col_idx, color)
+    for y in range(N):
+        temp = []
+        space_cnt = 0
+        for x in range(M):
+            if grid[y][x] == '.':
+                space_cnt += 1
+            else:
+                temp.append(grid[y][x])
 
-    # x가 큰거 부터 지워야 한다
-    pop_cord.sort(key=lambda x: -x[1])
+        for _ in range(space_cnt):
+            temp.append('.')
 
-    for y, x in pop_cord:
-        grid[y].pop(x)
-        grid[y].append('.')
+        nxt_grid[y].extend(temp)
 
-    if flag:
-        ans += 1
-############################################################
+    if not game: break
+    ans += 1
+
+    grid = nxt_grid
+
 print(ans)
